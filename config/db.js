c532c65f28b8ea688d961d576e2bc8799291e329
Harshit Raul <harshitraul15@gmail.com>
2024-09-26 22:45:39 +0530

    Change

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const URL = "mongodb+srv://root:root@cluster0.ca2cg9i.mongodb.net/ESchool?retryWrites=true&w=majority&appName=Cluster0";
+const URL = "mongodb://127.0.0.1:27017/ESchool?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6";
 
 mongoose.connect(URL).then(()=> console.log("!!MongoDB Connected!!")).catch((error)=>console.log(error));
