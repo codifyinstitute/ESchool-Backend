474ff92587df3ed303b12dfd5fc3779b087be243
HarshitRaul24 <harshitraul15@gmail.com>
2024-10-02 12:40:57 +0530

    Bug Removed

--- controller/enquiryController.js
+++ controller/enquiryController.js
@@ -86,7 +86,7 @@ exports.updateEnquiry = async (req,res) =>{
             return res.status(404).json({ message: "Enquiry Not Found." });
         }
 
-        res.status(200).json({message:"Enquiry Updated successfully."}, enquiry);
+        res.status(200).json({message:"Enquiry Updated successfully.", enquiry});
     } catch (error) {
         res.status(400).json({message: error.message});
         console.log(error);
