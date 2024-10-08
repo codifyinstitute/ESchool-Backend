3907bbd66c375502a0473dd0aae7c7c21818c4f3
HarshitRaul24 <harshitraul15@gmail.com>
2024-10-08 09:26:03 +0530

    Changes

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { login, changePassword } = require('../controller/loginController'); 
+const { login, changePassword, getAllLogins } = require('../controller/loginController'); 
 
 // Login route
 router.post('/login', login);
+
+// Get All Data
+router.get('/all', getAllLogins);
 
 // Change password route
 router.put('/change-password', changePassword);
