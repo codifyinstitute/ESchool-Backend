a75e461c55f1b96b0c295db81c2e3d501cb4774a
codifyinstitute <codifyinstitute@gmail.com>
2024-10-21 16:07:04 +0530

    All

--- controller/houseController.js
+++ controller/houseController.js
@@ -39,6 +39,7 @@ exports.createHouse = async (req, res) => {
         res.status(201).json(newHouse);
     } catch (err) {
         res.status(500).json({ message: "Error creating house", error: err.message });
+        console.log(err); 
     }
 };
 
